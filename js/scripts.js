e7c213098bd5905db2905492b4fdab2b8cf45c88
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-04-05 18:01:22 -0700

    add input for name and number

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    const personInput = $("input#person").val();
+    const phone-numberInput = $(input#number).val();
+
+
+    $(".person").append("blah blah");
+    $(".phone-number").append("blah blah");
+
+    event.();
+  });
+});
